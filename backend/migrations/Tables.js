b745238a2385d924c942240e6d27e3d1eaf124a3
FarukAli02 <160036148+FarukAli02@users.noreply.github.com>
2025-06-02 00:37:20 +0500

    Tables.js

--- backend/migrations/Tables.js
+++ backend/migrations/Tables.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
     .createTable('users', function (table) {
       table.increments('id').primary();
@@ -7,36 +7,9 @@ exports.up = function(knex) {
       table.string('password').notNullable();
       table.timestamps(true, true);
     })
-    .createTable('categories', function (table) {
+    .createTable('students', function (table) {
       table.increments('id').primary();
       table.string('name').notNullable();
-      table.text('note');
-      table.string('image'); // Added image field
-      table.timestamps(true, true);
-    })
-    .createTable('products', function (table) {
-      table.increments('id').primary();
-      table.string('name').notNullable();
-      table.text('description').notNullable();
-      table.decimal('price', 10, 2).notNullable();
-      table.integer('category_id').unsigned().references('id').inTable('categories').onDelete('CASCADE');
-      table.string('image'); // Added image field
-      table.timestamps(true, true);
-    })
-    .createTable('inventory', function (table) {
-      table.increments('id').primary();
-      table.integer('product_id').unsigned().references('id').inTable('products').onDelete('CASCADE');
-      table.integer('quantity').notNullable();
-      table.string('location').notNullable();
-      table.string('image'); // Added image field
-      table.timestamps(true, true);
-    });
-};
-
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('inventory')
-    .dropTableIfExists('products')
-    .dropTableIfExists('categories')
-    .dropTableIfExists('users');
-};
+      table.integer('age').notNullable();
+      table.string('course').notNullable();
+      table.string('regid').notNullable().unique(); // Assuming
